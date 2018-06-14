70d8b713320935eb8752eb1bbe4d874a6c473a61
Shirshendu Bhowmick <shirshendu_0@yahoo.co.in>
2018-06-14 16:17:28 +0530

    CSV Parsing added

--- extract.py
+++ extract.py
@@ -1,15 +1,18 @@
 from datetime import date, timedelta
 import urllib.request
 import zipfile
+import csv
 
 
 class ExtractBhavCopy:
     baseUrl = "https://www.bseindia.com/download/BhavCopy/Equity/"
     finalUrl = None
+    csvData = []
+    yesterdayDate = None
 
     def __init__(self):
-        yesterdayDate = (date.today() - timedelta(1)).strftime("%d%m%y")
-        zipFileName = "EQ" +  yesterdayDate + "_CSV.ZIP"
+        self.yesterdayDate = (date.today() - timedelta(1)).strftime("%d%m%y")
+        zipFileName = "EQ" +  self.yesterdayDate + "_CSV.ZIP"
         self.finalUrl = self.baseUrl + zipFileName
         print(self.finalUrl)
         try:
@@ -20,9 +23,17 @@ class ExtractBhavCopy:
             zipRef = zipfile.ZipFile("zipFiles/" + zipFileName, 'r')
             zipRef.extractall("csvExtract")
             zipRef.close()
+            self.parseCSV()
         except:
             print("Error extracting ZIP file")
-
+    
+    def parseCSV(self):
+        csvFileName = "EQ" + self.yesterdayDate + ".CSV"
+        with open("csvExtract/" + csvFileName, 'r') as csvFile:
+            csvData = csv.DictReader(csvFile)
+            for row in csvData:
+                self.csvData.append(row)
+        print(self.csvData)
 
 
 data = ExtractBhavCopy()
